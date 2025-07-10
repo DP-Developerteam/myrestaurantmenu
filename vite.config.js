3b783c358c61dd227fc325ded857306186eab436
Diego Pérez Rodríguez <diegoperez.developer@gmail.com>
2025-07-10 22:19:15 +0200

    vercel preparation

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,22 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux', 'redux', 'redux-persist', 'redux-thunk'],
+          animations: ['gsap'],
+          i18n: ['react-i18next', 'i18next-browser-languagedetector'],
+          utils: ['redux-persist-transform-filter']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
   }
 });
