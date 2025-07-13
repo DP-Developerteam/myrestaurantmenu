d048272857457aa18dd9a274fed69d86254238e8
Diego Pérez Rodríguez <diegoperez.developer@gmail.com>
2025-07-13 22:52:37 +0200

    com-se-tabs

---
+++ src/components/sections/TextImage.jsx
@@ -0,0 +1,98 @@
+// Import styles and libraries
+import '../../styles/com-se.tabs.scss';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+const TextImage = ({title, tabs, tabsContent}) => {
+    // States for translations
+    const { t } = useTranslation();
+
+    // State to track which tab is currently active (index-based)
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+    // Get the currently active content based on the active tab index
+    const activeContent = tabsContent && tabsContent[activeTabIndex];
+
+    // Function to handle tab click
+    const handleTabClick = (index) => {
+        setActiveTabIndex(index);
+    };
+
+    // Render tabs
+    const renderTabs = () => {
+        return (
+            <ul className='tabs-container' >
+                {tabsContent.map((content, index) => (
+                    <li
+                        key={index}
+                        className={`tab ${activeTabIndex === index ? 'active' : ''}`}
+                        onClick={() => handleTabClick(index)}
+                        style={{ cursor: 'pointer' }}
+                    >
+                        {t(content.title)}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+    // Render bullets
+    const renderBullets = () => {
+        return (
+            <>
+                <ul className='bullets-container'>
+                    {Object.entries(activeContent.bullets).map(([key, bullet]) => (
+                        <li className='bullet' key={key}>
+                            <span className='icon'>o</span> {t(bullet)}
+                        </li>
+                    ))}
+                </ul>
+            </>
+        )
+    }
+
+    // Render buttons
+    const renderButtons = () => {
+        return (
+            <div className='buttons-container'>
+                {Object.entries(activeContent.buttons).map(([key, button]) => (
+                    <button className={button.cssClass} key={key}>
+                        {t(button.content)}
+                    </button>
+                ))}
+            </div>
+        )
+    }
+
+    // Render image
+    const renderImage = () => {
+        return (
+            <div className='image-container'>
+                <img
+                    src={activeContent.image.src || activeContent.image}
+                    alt={activeContent.image.alt || 'Image'}
+                    width={activeContent.image?.width}
+                    height={activeContent.image?.height}
+                />
+            </div>
+        )
+    }
+
+
+    return (
+        <section className='section section-tabs'>
+            {title && <h3 className='title'>{t(title)}</h3> }
+            {tabs && tabsContent && renderTabs()}
+            <div className='content-container'>
+                <div className='text-container'>
+                    {activeContent && activeContent.bullets && renderBullets()}
+                    {activeContent && activeContent.buttons && renderButtons()}
+                </div>
+                {activeContent.image && renderImage()}
+            </div>
+        </section>
+    )
+}
+
+export default TextImage
--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -3,123 +3,256 @@ import '../../App.scss';
 // Import components
 import SectionHero from '../../components/sections/Hero';
 import SectionCardsFeatures from '../../components/sections/CardsBenefits';
+import TextImage from '../../components/sections/TextImage';
 // Import icons & images
 import SliderHero01 from '../../assets/img/qr-code.webp';
 
 const Home = () => {
 
-    const cardsData01 = [
-        {
-            id: 1,
-            isOnClick: true,
-            text: 'Flip card',
-            image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
+    const SectionCardsFeatures01 = {
+        'cardsData': [
+            {
+                id: 1,
+                isOnClick: true,
+                text: 'Flip card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 2,
+                isOnClick: true,
+                text: 'Expand card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 3,
+                isOnClick: true,
+                text: 'Static card 1',
+                image: SliderHero01,
+                description: 'Description'
+            },
+            {
+                id: 4,
+                isOnClick: true,
+                text: 'Static card 2',
+                image: SliderHero01,
+            },
+            {
+                id: 5,
+                isOnClick: true,
+                text: 'Static card 3',
+                image: SliderHero01,
+            },
+            {
+                id: 6,
+                isOnClick: true,
+                text: 'Static card 4',
+                image: SliderHero01,
+            },
+            {
+                id: 7,
+                cssClass: 'card-red',
+                isOnClick: true,
+                text: 'Static card RED',
+                image: SliderHero01,
+                description: 'Description'
+            },
+        ]
+    };
+    const SectionCardsFeatures02 = {
+        'text': {
+            'title': 'We work with companies like yours',
         },
-        {
-            id: 2,
-            isOnClick: true,
-            text: 'Expand card',
-            image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
-        },
-        {
-            id: 3,
-            isOnClick: true,
-            text: 'Static card 1',
-            image: SliderHero01,
-            description: 'Description'
-        },
-        {
-            id: 4,
-            isOnClick: true,
-            text: 'Static card 2',
-            image: SliderHero01,
-        },
-        {
-            id: 5,
-            isOnClick: true,
-            text: 'Static card 3',
-            image: SliderHero01,
-        },
-        {
-            id: 6,
-            isOnClick: true,
-            text: 'Static card 4',
-            image: SliderHero01,
-        },
-        {
-            id: 7,
-            cssClass: 'card-red',
-            isOnClick: true,
-            text: 'Static card RED',
-            image: SliderHero01,
-            description: 'Description'
-        },
-    ];
-    const cardsData02 = [
-        {
-            id: 1,
-            isOnClick: true,
-            text: 'Flip card',
-            image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
-        },
-        {
-            id: 2,
-            isOnClick: true,
-            text: 'Expand card',
-            image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
-        },
-        {
-            id: 3,
-            isOnClick: true,
-            text: 'Static card 1',
-            image: SliderHero01,
-            description: 'Description'
-        },
-        {
-            id: 4,
-            isOnClick: true,
-            text: 'Static card 2',
-            image: SliderHero01,
-        },
-    ];
-    const cardsData03 = [
-        {
-            id: 1,
-            isOnClick: true,
-            text: 'Flip card',
-            // image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
-        },
-        {
-            id: 2,
-            isOnClick: true,
-            text: 'Expand card',
-            image: SliderHero01,
-            description: 'Este es un texto de descripción que aparece al hacer clic.',
-        },
-        {
-            id: 3,
-            isOnClick: true,
-            text: 'Static card 1',
-            image: SliderHero01,
-            description: 'Description'
-        },
-    ];
-    const textFeatures = {
-        title: 'We work with companies like yours',
-    }
+        'cardsData': [
+            {
+                id: 1,
+                isOnClick: true,
+                text: 'Flip card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 2,
+                isOnClick: true,
+                text: 'Expand card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 3,
+                isOnClick: true,
+                text: 'Static card 1',
+                image: SliderHero01,
+                description: 'Description'
+            },
+            {
+                id: 4,
+                isOnClick: true,
+                text: 'Static card 2',
+                image: SliderHero01,
+            },
+        ],
+        'cssClass': 'section-red-4-cards'
+    };
+    const SectionTextImage01 = {
+        'title': "My Component",
+        'tabs': false,
+        'tabsContent': [
+            {
+                bullets: {
+                    '01': 'Display products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                buttons: {
+                    first: {
+                        content: 'Check the live demo now',
+                        cssClass: 'btn-solid-light btn-inner-shadow',
+                        to: 'demo'
+                    },
+                    second: {
+                        content: 'Start trial for free',
+                        cssClass: 'btn-solid-red',
+                        to: 'promo'
+                    }
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            }
+        ]
+    };
+    const SectionCardsFeatures03 = {
+        'cardsData': [
+            {
+                id: 1,
+                isOnClick: true,
+                text: 'Flip card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 2,
+                isOnClick: true,
+                text: 'Expand card',
+                image: SliderHero01,
+                description: 'Este es un texto de descripción que aparece al hacer clic.',
+            },
+            {
+                id: 3,
+                isOnClick: true,
+                text: 'Static card 1',
+                image: SliderHero01,
+                description: 'Description'
+            },
+        ]
+    };
+    const SectionTextImage02 = {
+        'tabs': true,
+        'tabsContent': [
+            {
+                title: 'Clients CMS',
+                bullets: {
+                    '01': 'Display products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            },
+            {
+                title: 'Manager CMS',
+                bullets: {
+                    '01': 'MANAGER products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            },
+            {
+                title: 'Customization',
+                bullets: {
+                    '01': 'Customization products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            },
+            {
+                title: 'Syncronization',
+                bullets: {
+                    '01': 'Syncronization products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            },
+            {
+                title: 'Push notifications',
+                bullets: {
+                    '01': 'Display products with Photos, videos, or lists — your menu adapts to every customer.',
+                    '02': 'Smart display adapts to internet speed. Enhancing user experience assuring a smooth experience.',
+                    '03': 'Built-in marketing tools turn clicks into real orders.',
+                    '04': 'From daily specials to holiday favorites, everything is flexible, immediate, and built to sell.',
+                    '05': 'Built-in marketing tools turn clicks into real orders.'
+
+                },
+                image: {
+                    src: SliderHero01,
+                    alt: 'Description',
+                    width: 300,
+                    height: 300,
+                }
+            }
+        ]
+    };
 
     return (
         <div className='page page-home'>
             <SectionHero />
-            <SectionCardsFeatures cardsData={cardsData01} />
-            <SectionCardsFeatures cardsData={cardsData03} />
-            <SectionCardsFeatures text={textFeatures} cardsData={cardsData02} cssClass='section-red-4-cards' />
-            <SectionHero />
+            <SectionCardsFeatures cardsData={SectionCardsFeatures01.cardsData} />
+            <SectionCardsFeatures text={SectionCardsFeatures02.text} cardsData={SectionCardsFeatures02.cardsData} cssClass={SectionCardsFeatures02.cssClass} />
+            <TextImage title={SectionTextImage01.title} tabs={SectionTextImage01.tabs} tabsContent={SectionTextImage01.tabsContent} />
+            <SectionCardsFeatures cardsData={SectionCardsFeatures03.cardsData} />
+            <TextImage tabs={SectionTextImage02.tabs} tabsContent={SectionTextImage02.tabsContent} />
         </div>
     )
 }
